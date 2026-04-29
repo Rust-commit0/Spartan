4b44b33fc48c9f60fc7473a18765ab8892bbe626
Anzar <mirza.baig@ethara.ai>
2026-04-29 17:07:56 +0530

    Commit 0: stub spartan source

--- src/random.rs
+++ src/random.rs
@@ -2,27 +2,17 @@ use super::scalar::Scalar;
 use super::transcript::ProofTranscript;
 use merlin::Transcript;
 use rand::rngs::OsRng;
-
 pub struct RandomTape {
-  tape: Transcript,
+    tape: Transcript,
 }
-
 impl RandomTape {
-  pub fn new(name: &'static [u8]) -> Self {
-    let tape = {
-      let mut csprng: OsRng = OsRng;
-      let mut tape = Transcript::new(name);
-      tape.append_scalar(b"init_randomness", &Scalar::random(&mut csprng));
-      tape
-    };
-    Self { tape }
-  }
-
-  pub fn random_scalar(&mut self, label: &'static [u8]) -> Scalar {
-    self.tape.challenge_scalar(label)
-  }
-
-  pub fn random_vector(&mut self, label: &'static [u8], len: usize) -> Vec<Scalar> {
-    self.tape.challenge_vector(label, len)
-  }
+    pub fn new(name: &'static [u8]) -> Self {
+        panic!("STUB: not implemented");
+    }
+    pub fn random_scalar(&mut self, label: &'static [u8]) -> Scalar {
+        panic!("STUB: not implemented");
+    }
+    pub fn random_vector(&mut self, label: &'static [u8], len: usize) -> Vec<Scalar> {
+        panic!("STUB: not implemented");
+    }
 }
--- src/timer.rs
+++ src/timer.rs
@@ -6,83 +6,44 @@ use core::sync::atomic::AtomicUsize;
 use core::sync::atomic::Ordering;
 #[cfg(feature = "profile")]
 use std::time::Instant;
-
 #[cfg(feature = "profile")]
 pub static CALL_DEPTH: AtomicUsize = AtomicUsize::new(0);
-
 #[cfg(feature = "profile")]
 pub struct Timer {
-  label: String,
-  timer: Instant,
+    label: String,
+    timer: Instant,
 }
-
 #[cfg(feature = "profile")]
 impl Timer {
-  #[inline(always)]
-  pub fn new(label: &str) -> Self {
-    let timer = Instant::now();
-    CALL_DEPTH.fetch_add(1, Ordering::Relaxed);
-    let star = "* ";
-    println!(
-      "{:indent$}{}{}",
-      "",
-      star,
-      label.yellow().bold(),
-      indent = 2 * CALL_DEPTH.fetch_add(0, Ordering::Relaxed)
-    );
-    Self {
-      label: label.to_string(),
-      timer,
+    #[inline(always)]
+    pub fn new(label: &str) -> Self {
+        panic!("STUB: not implemented");
     }
-  }
-
-  #[inline(always)]
-  pub fn stop(&self) {
-    let duration = self.timer.elapsed();
-    let star = "* ";
-    println!(
-      "{:indent$}{}{} {:?}",
-      "",
-      star,
-      self.label.blue().bold(),
-      duration,
-      indent = 2 * CALL_DEPTH.fetch_add(0, Ordering::Relaxed)
-    );
-    CALL_DEPTH.fetch_sub(1, Ordering::Relaxed);
-  }
-
-  #[inline(always)]
-  pub fn print(msg: &str) {
-    CALL_DEPTH.fetch_add(1, Ordering::Relaxed);
-    let star = "* ";
-    println!(
-      "{:indent$}{}{}",
-      "",
-      star,
-      msg.to_string().green().bold(),
-      indent = 2 * CALL_DEPTH.fetch_add(0, Ordering::Relaxed)
-    );
-    CALL_DEPTH.fetch_sub(1, Ordering::Relaxed);
-  }
+    #[inline(always)]
+    pub fn stop(&self) {
+        panic!("STUB: not implemented");
+    }
+    #[inline(always)]
+    pub fn print(msg: &str) {
+        panic!("STUB: not implemented");
+    }
 }
-
 #[cfg(not(feature = "profile"))]
 pub struct Timer {
-  _label: String,
+    _label: String,
 }
-
 #[cfg(not(feature = "profile"))]
 impl Timer {
-  #[inline(always)]
-  pub fn new(label: &str) -> Self {
-    Self {
-      _label: label.to_string(),
+    #[inline(always)]
+    pub fn new(label: &str) -> Self {
+        panic!("STUB: not implemented");
     }
-  }
-
-  #[inline(always)]
-  pub fn stop(&self) {}
-
-  #[inline(always)]
-  pub fn print(_msg: &str) {}
+    #[inline(always)]
+    pub fn stop(&self) {
+        panic!("STUB: not implemented");
+    }
+    #[inline(always)]
+    pub fn print(_msg: &str) {
+        panic!("STUB: not implemented");
+    }
 }
